f741b7ae705e902ff52487f9215a0b74c7fdfa53
Graeme <graeme.barnes@cloudbi.co.za>
2013-07-22 22:12:16 +0200

    created basic scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MikeHartlApp::Application.config.secret_key_base = '2a1e7e7f1ea7f8252110b88649842dc1e360050cd5c8aaa14b37a8a3d56ba9de66b2b6f8f8d807613dc954beb174aac9ea0423c9a3089113d8649d382fafb54f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MikeHartlApp::Application.config.session_store :cookie_store, key: '_Mike_Hartl_app_session'
